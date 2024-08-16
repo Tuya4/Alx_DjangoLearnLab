b879405adeaef7bf2bbce322c1453f0f21fc9dd0
Mohan <mohansang04@gmail.com>
2024-08-16 06:42:07 +0300

    Implemented Advanced Model Relationships in Django

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,31 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+def query_books_by_author(author_name):
+    books = Book.objects.filter(author_name=author_name)
+    return books
+
+def  list_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    return books
+
+def retrieve_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = Librarian.objects.get(library=library)
+        return librarian
+    except Librarian.DoesNotExist:
+        return None
+    
+    # if __name__ == "__main__":
+    #     author_books = query_books_by_author("George Orwell")
+    #     print(f"Books by John Doe: {[book.title for book in author_books]}")
+
+    #     library_books = list_books_in_library("Central Library")
+    #     print(f"Books in Central Library: {[book.title for book in library_books]}")
+
+    #     librarian = retrieve_librarian_for_library("Central Library")
+    #     if librarian:
+    #         print(f"Librarian of Central Library: {librarian.name}")
+    #     else:
+    #         print("No librarian found for Central Library")
