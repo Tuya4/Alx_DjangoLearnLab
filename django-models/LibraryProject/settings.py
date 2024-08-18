80c190a323c0910506cc482b35aa7115dbae0656
Tuya4 <mohansang04@gmail.com>
2024-08-18 05:32:10 +0300

    Django Views and URL Configuration

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR, 'templates' ], # Add this line to the list of templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
