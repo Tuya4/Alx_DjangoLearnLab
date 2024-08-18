06a843c3345aa8698e0913d75ef662297d0fd76e
Tuya4 <mohansang04@gmail.com>
2024-08-18 05:01:49 +0300

    Implemented Advanced Model Relationships in Django

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -30,9 +30,9 @@ for book in books:
 # Query all books by a specific author
 author_name = 'J.K. Rowling'
 author = Author.objects.get(name=author_name)
-books = author.book_set.all()
+books_by_author = Book.objects.filter(author=author)
 print(f'Books by {author_name}:')
-for book in books:
+for book in books_by_author:
     print(book.title)    
 
 # Query all books in the library
