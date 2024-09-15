f61d67a1917d76d858ec4eef9de8612e691db594
Tuya4 <mohansang04@gmail.com>
2024-09-15 17:41:53 +0300

    Adding Functionality to Blog Posts

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -20,3 +20,18 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
+    
+# class Tag(models.Model):
+#     name = models.CharField(max_length=100, unique=True)
+
+#     def __str__(self):
+#         return self.name
+# class Post(models.Model):
+#     title = models.CharField(max_length=200)
+#     content = models.TextField()
+#     published_date = models.DateTimeField(auto_now_add=True)
+#     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+#     tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+
+#     def __str__(self):
+#         return self.title        
