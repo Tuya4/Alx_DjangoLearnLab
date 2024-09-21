e752254ba3b04b0e6b852e0ab018c4991c8dc806
Tuya4 <mohansang04@gmail.com>
2024-09-21 12:34:39 +0300

    Project setup and user authentication

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,13 +4,15 @@ from django.contrib.auth import get_user_model
 from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
-    # define password as CharField to ensure proper validation
-    password = serializers.CharField(write_only=True, min_length=8, max_length=15)
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(write_only=True, min_length=8)
+    email = serializers.EmailField()
+    bio = serializers.CharField(allow_blank=True, required=False)
+    profile_picture = serializers.ImageField(required=False)
     
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         # Use get_user_model to ensure compatibility with custom user models
