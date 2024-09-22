1bb88d9d0c72a4329dc1985637696b308b11987e
Tuya4 <mohansang04@gmail.com>
2024-09-22 08:15:17 +0300

    Post and comments functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -20,5 +20,6 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)        
